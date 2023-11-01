950f55b6a7c4f1bb56ed0cce8db4b3a1ce23dc3d
Victor <vmcabredo@gmail.com>
2023-11-01 16:20:00 -0300

    minor changes

--- vue-frontend/src/components/MyComponent.vue
+++ vue-frontend/src/components/MyComponent.vue
@@ -4,15 +4,23 @@ import { ref, watch } from "vue"
 const requestId = ref(1);
 const requestData = ref(null);
 
-const dataEndpoint = "/api/data";
+const dataEndpoint = import.meta.env.VITE_LOCAL_DEV_BACKEND_DATA_ENDPOINT || "/api/data";
 
 async function fetchData() {
     requestData.value = null;
-    const res = await fetch(dataEndpoint);
-    requestData.value = await res.json();
-}
+    try {
+        const res = await fetch(dataEndpoint);
+        console.log(res);
+        if (!res.ok) {
+            console.warn(`Backend API respondend with status code ${res.status}`);
+        }
+        else requestData.value = await res.json();
+    } catch (error) {
+        console.error(error)
+    }
+}; fetchData();
 
-watch(fetchData, {imediate: true});
+watch(requestId, fetchData);
 </script>
 
 <template>
